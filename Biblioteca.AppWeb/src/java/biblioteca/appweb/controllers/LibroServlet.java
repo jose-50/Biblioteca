653a985da6f5a3e7bf915dcbafc9c008a2eeff56
Mariela Blanco <bs21002ts@esfe.agape.edu.sv>
2022-08-24 09:05:28 -0600

    controladores

--- Biblioteca.AppWeb/src/java/biblioteca/appweb/controllers/LibroServlet.java
+++ Biblioteca.AppWeb/src/java/biblioteca/appweb/controllers/LibroServlet.java
@@ -1,33 +1,45 @@
-
-package practicaservlets;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package biblioteca.appweb.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ *
+ * @author Alumno
+ */
+@WebServlet(name = "LibroServlet", urlPatterns = {"/LibroServlet"})
+public class LibroServlet extends HttpServlet {
 
-public class Operaciones extends HttpServlet {
-
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            
-            double num1 = Double.parseDouble(request.getParameter("txNum1"));
-            double num2 = Double.parseDouble (request.getParameter("txtNum2"));
-            double suma = num1 + num2 ; 
-            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>");            
+            out.println("<title>Servlet LibroServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>  La suma de los numeros es " + suma + " </h1");
+            out.println("<h1>Servlet LibroServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
